8c69b3a4c25992fa77a87521c8f6aa35b193a78a
yidane <yidane@163.com>
2018-03-21 13:27:25 +0800

    update test case

--- leetcode/problems/ballfall_test.go
+++ leetcode/problems/ballfall_test.go
@@ -18,15 +18,15 @@ func Test_ballfall(t *testing.T) {
 	}
 
 	Convey("计算球落地弹起的高度", t, func() {
+		Convey("leetcode/problems/ballfall_test.go", func() {})
 		for i, tt := range tests {
 			fmt.Println(i)
 			Convey(strconv.Itoa(i), func() {
 				h, tot := ballfall(tt.h, tt.t)
-				So(h, ShouldBeGreaterThan, 0)
-				So(tot, ShouldBeGreaterThan, 0)
+				h1, tot1 := ballfall1(tt.h, tt.t)
+				So(h, ShouldEqual, h1)
+				So(tot, ShouldEqual, tot1)
 			})
-
-			//fmt.Println(i, tt)
 		}
 	})
 }
--- leetcode/problems/longestPalindromicSubstring_test.go
+++ leetcode/problems/longestPalindromicSubstring_test.go
@@ -1,16 +1,31 @@
 package problems
 
 import (
+	"strconv"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func Test_longestPalindrome(t *testing.T) {
-	Convey("寻找最大回环子字符串", t, func() {
-		So(longestPalindrome("abab"), ShouldEqual, "aba")
-		So(longestPalindrome("aba"), ShouldEqual, "aba")
-		So(longestPalindrome("a"), ShouldEqual, "a")
-		So(longestPalindrome("ccc"), ShouldEqual, "ccc")
+	tests := []struct {
+		arg  string
+		want string
+	}{
+		{arg: "abab", want: "aba"},
+		{arg: "aba", want: "aba"},
+		{arg: "a", want: "a"},
+		{arg: "ccc", want: "ccc"},
+	}
+
+	Convey("leetcode/problems/longestPalindromicSubstring_test", t, func() {
+		Convey("寻找回环数", func() {
+
+		})
+		for i, tt := range tests {
+			Convey(strconv.Itoa(i), func() {
+				So(longestPalindrome(tt.arg), ShouldEqual, tt.want)
+			})
+		}
 	})
 }
