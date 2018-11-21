61541e8e8930f8648b0ee885ae1710d0e4fc0b38
yidane <yidane@163.com>
2018-11-21 17:52:52 +0800

    add all leetcode problem file and test file

---
+++ leetcode/problems/27.remove-element_1.go
@@ -0,0 +1,21 @@
+package problems
+
+import "fmt"
+
+/*
+
+ */
+
+func removeElement(nums []int, val int) int {
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+
+	fmt.Println(nums[:i])
+
+	return i
+}
