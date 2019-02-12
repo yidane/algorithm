91b04a04eeb0c3a7139222b27f77f66fc0e4bbb0
yidane <yidane@163.com>
2019-02-12 16:16:13 +0800

    9.palindrome number

--- leetcode/problems/100/9.palindrome-number_1.go
+++ leetcode/problems/100/9.palindrome-number_1.go
@@ -1,1 +1,28 @@
 package problems
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	if x < 10 {
+		return true
+	}
+
+	if x%10 == 0 {
+		return false
+	}
+
+	o := x
+	n := 0
+
+	for x >= 10 {
+		i := x % 10
+		n = n*10 + i
+		x = (x - i) / 10
+	}
+
+	n = n*10 + x
+
+	return o == n
+}
