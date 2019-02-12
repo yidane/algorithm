37377d42da1672b82a89ea991838c1a4f584ba85
yidane <yidane@163.com>
2019-02-12 14:49:43 +0800

    longest common prefix

--- leetcode/problems/100/14.longest-common-prefix_1.go
+++ leetcode/problems/100/14.longest-common-prefix_1.go
@@ -1,1 +1,41 @@
 package problems
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	if len(strs) == 1 {
+		return strs[0]
+	}
+
+	var rs []rune
+	i := 0
+	var si rune = -1
+l:
+	for {
+		for _, str := range strs {
+			if len(str) == 0 {
+				return ""
+			}
+
+			if len(str) == i {
+				break l
+			}
+
+			if si == -1 {
+				si = rune(str[i])
+			} else {
+				if si != rune(str[i]) {
+					break l
+				}
+			}
+		}
+
+		rs = append(rs, si)
+		si = -1
+		i++
+	}
+
+	return string(rs)
+}
