51a1c7068536a16725dbbd23ecfc410c80219cb8
yidane <yidane@163.com>
2019-02-12 17:25:01 +0800

    58.length-of-last-word_1

--- leetcode/problems/100/58.length-of-last-word_1_test.go
+++ leetcode/problems/100/58.length-of-last-word_1_test.go
@@ -1,1 +1,20 @@
 package problems
+
+import "testing"
+
+func Test_lengthOfLastWord(t *testing.T) {
+	tests := map[string]int{
+		"":             0,
+		"yidane":       6,
+		"yidane ":      6,
+		"hello world":  5,
+		"hello world ": 5,
+	}
+	for k, v := range tests {
+		t.Run(k, func(t *testing.T) {
+			if got := lengthOfLastWord(k); got != v {
+				t.Errorf("lengthOfLastWord(%s) = %v, want %v", k, got, v)
+			}
+		})
+	}
+}
