0834fe0f0fcff013120fea19f3a26070b3653dee
yidane <yidane@163.com>
2019-02-13 00:41:50 +0800

    /53.maximum-subarray_1.go

--- leetcode/problems/100/53.maximum-subarray_1.go
+++ leetcode/problems/100/53.maximum-subarray_1.go
@@ -1,1 +1,21 @@
 package problems
+
+func maxSubArray(nums []int) int {
+	//todo
+	//if len(nums) == 0 {
+	//	return 0
+	//}
+	//
+	//max := nums[0]
+	//tmax := max
+	//for i := 0; i < len(nums); i++ {
+	//	tmax += nums[i]
+	//	if tmax > max {
+	//		max = tmax
+	//	}
+	//}
+	//
+	////1 -1 -2 1000
+
+	return 1
+}
