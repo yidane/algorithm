753f2cb9bc6445f46b6b18686a21e31034fceb1d
yidane <yidane@163.com>
2019-02-12 17:07:38 +0800

    35.search-insert-position_1

--- leetcode/problems/100/35.search-insert-position_1.go
+++ leetcode/problems/100/35.search-insert-position_1.go
@@ -1,1 +1,14 @@
 package problems
+
+func searchInsert(nums []int, target int) int {
+	index := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] >= target {
+			return i
+		} else {
+			index++
+		}
+	}
+
+	return index
+}
