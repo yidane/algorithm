cc74fa2586ec2428d36048a71592d60c0c9e27fd
yidane <yidane@163.com>
2019-02-13 14:37:38 +0800

    69.sqrtx_1

--- leetcode/problems/100/69.sqrtx_1.go
+++ leetcode/problems/100/69.sqrtx_1.go
@@ -1,1 +1,51 @@
 package problems
+
+import "math"
+
+func mySqrt(x int) int {
+	if x == 0 {
+		return 0
+	}
+
+	if x < 4 {
+		return 1
+	}
+
+	m := 2
+	for i := 2; i <= x; i++ {
+		j := x / i
+
+		switch {
+		case j < i:
+			return m
+		case j == i:
+			return i
+		case j > i:
+			m = i
+		}
+	}
+
+	return -1
+}
+
+//todo nice
+func mySqrt1(x int) int {
+	z := 1.0
+	prevZ := z
+	equalityThreshold := 1e-9
+	firstTime := true
+	for true {
+		z -= ((z * z) - float64(x)) / (2 * z)
+		if firstTime {
+			prevZ = z
+			firstTime = false
+		} else {
+			curZ := prevZ - z
+			prevZ = z
+			if math.Abs(curZ) <= equalityThreshold {
+				return int(z)
+			}
+		}
+	}
+	return int(z)
+}
--- leetcode/problems/100/69.sqrtx_1_test.go
+++ leetcode/problems/100/69.sqrtx_1_test.go
@@ -1,1 +1,23 @@
 package problems
+
+import "testing"
+
+func Test_mySqrt(t *testing.T) {
+	type args struct {
+		x    int
+		want int
+	}
+	tests := []args{
+		{x: 2, want: 1},
+		{x: 3, want: 1},
+		{x: 4, want: 2},
+		{x: 8, want: 2},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := mySqrt(tt.x); got != tt.want {
+				t.Errorf("mySqrt(%v) = %v, want %v", tt.x, got, tt.want)
+			}
+		})
+	}
+}
