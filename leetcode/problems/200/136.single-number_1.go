6651436c26c20299ec84043aa58d9f1992607853
yidane <yidane@163.com>
2019-02-13 09:42:07 +0800

    136.single-number_1

--- leetcode/problems/200/136.single-number_1.go
+++ leetcode/problems/200/136.single-number_1.go
@@ -1,1 +1,9 @@
 package problems
+
+func singleNumber(nums []int) int {
+	for i := 1; i < len(nums); i++ {
+		nums[0] = nums[0] ^ nums[i]
+	}
+
+	return nums[0]
+}
--- leetcode/problems/200/136.single-number_1_test.go
+++ leetcode/problems/200/136.single-number_1_test.go
@@ -1,1 +1,21 @@
 package problems
+
+import "testing"
+
+func Test_singleNumber(t *testing.T) {
+	tests := []struct {
+		name string
+		args []int
+		want int
+	}{
+		{args: []int{2, 2, 1}, want: 1},
+		{args: []int{4, 1, 2, 1, 2}, want: 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
