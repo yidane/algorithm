8a393d451d39bf749e234b42cdec5fb458c392d4
yidane <yidane@163.com>
2019-02-13 15:26:54 +0800

    283.move-zeroes_1

--- leetcode/problems/300/283.move-zeroes_1.go
+++ leetcode/problems/300/283.move-zeroes_1.go
@@ -1,1 +1,41 @@
 package problems
+
+/*
+Input: [0,1,0,3,12]
+0 1 0 3 12
+1 0 0 3 12
+1 0 3 0 12
+1 0 3 12 0
+1 3 0 12 0
+1 3 12 0 0
+Output: [1,3,12,0,0]
+*/
+func moveZeroes(nums []int) {
+	c := 0
+	for i := 0; i < len(nums); i++ {
+		if i+c == len(nums) {
+			break
+		}
+
+		if nums[i] == 0 {
+			for j := i; j < len(nums)-c-1; j++ {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+			c++
+
+			if nums[i] == 0 {
+				i--
+			}
+		}
+	}
+}
+
+func moveZeroes1(nums []int) {
+	nonZeros := 0
+	for curr := 0; curr < len(nums); curr++ {
+		if nums[curr] != 0 {
+			nums[nonZeros], nums[curr] = nums[curr], nums[nonZeros]
+			nonZeros++
+		}
+	}
+}
--- leetcode/problems/300/283.move-zeroes_1_test.go
+++ leetcode/problems/300/283.move-zeroes_1_test.go
@@ -1,1 +1,29 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_moveZeroes(t *testing.T) {
+	type args struct {
+		nums  []int
+		wants []int
+	}
+	tests := []args{
+		{nums: []int{0, 1, 0, 3, 12}, wants: []int{1, 3, 12, 0, 0}},
+		{nums: []int{0, 0, 1}, wants: []int{1, 0, 0}},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			moveZeroes1(tt.nums)
+
+			fmt.Println(tt.nums)
+			for i := 0; i < len(tt.nums); i++ {
+				if tt.nums[i] != tt.wants[i] {
+					t.Fatalf("%v should be equal %v", tt.nums[i], tt.wants[i])
+				}
+			}
+		})
+	}
+}
