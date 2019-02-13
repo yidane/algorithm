b0701e3a88e80598063739ac5850fe8cb8b606ed
yidane <yidane@163.com>
2019-02-13 10:39:40 +0800

    189.rotate-array_1

--- leetcode/problems/200/189.rotate-array_1.go
+++ leetcode/problems/200/189.rotate-array_1.go
@@ -1,1 +1,51 @@
 package problems
+
+/*
+	1 2 3 4
+	4 1 2 3
+	3 4 1 2
+	2 3 4 1
+*/
+
+//k*n
+func rotate(nums []int, k int) {
+	if len(nums) == 0 {
+		return
+	}
+
+	k = k % len(nums)
+	l := len(nums)
+	for i := 0; i < k; i++ {
+		for j := l - 1; j > 0; j-- {
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+		}
+	}
+}
+
+//
+func rotate1(nums []int, k int) {
+	if len(nums) == 0 {
+		return
+	}
+
+	k = k % len(nums)
+	l := len(nums)
+
+	for i, j := 0, l-1; i < j; {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
+
+	for i, j := 0, k-1; i < j; {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
+
+	for i, j := k, l-1; i < j; {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
+}
--- leetcode/problems/200/189.rotate-array_1_test.go
+++ leetcode/problems/200/189.rotate-array_1_test.go
@@ -1,1 +1,25 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_rotate(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+		want []int
+	}
+	tests := []args{
+		{nums: []int{1, 2, 3, 4, 5, 6, 7}, k: 3, want: []int{5, 6, 7, 1, 2, 3, 4}},
+		{nums: []int{-1, -100, 3, 99}, k: 2, want: []int{3, 99, -1, -100}},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			rotate1(tt.nums, tt.k)
+
+			fmt.Println(tt.nums)
+		})
+	}
+}
