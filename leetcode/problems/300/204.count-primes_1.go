0e0c5db5ed9a3ad7574c9123192e9c9e807031ba
yidane <yidane@163.com>
2019-02-13 16:57:33 +0800

    204.count-primes_1

--- leetcode/problems/300/204.count-primes_1.go
+++ leetcode/problems/300/204.count-primes_1.go
@@ -1,1 +1,34 @@
 package problems
+
+//todo slow
+func countPrimes(n int) int {
+	if n <= 2 {
+		return 0
+	}
+
+	if n == 3 {
+		return 1
+	}
+
+	c := 1
+	primes := []int{2}
+	for i := 3; i < n; i = i + 2 {
+		isPrimes := true
+		for _, p := range primes {
+			if p*p > i {
+				break
+			}
+			if i%p == 0 {
+				isPrimes = false
+				break
+			}
+		}
+
+		if isPrimes {
+			primes = append(primes, i)
+			c++
+		}
+	}
+
+	return c
+}
--- leetcode/problems/300/204.count-primes_1_test.go
+++ leetcode/problems/300/204.count-primes_1_test.go
@@ -1,1 +1,27 @@
 package problems
+
+import (
+	"strconv"
+	"testing"
+)
+
+func Test_countPrimes(t *testing.T) {
+	type args struct {
+		n    int
+		want int
+	}
+	tests := []args{
+		{n: 2, want: 0},
+		{n: 10, want: 4},
+		{n: 12, want: 5},
+		{n: 13, want: 5},
+		{n: 999983, want: 5},
+	}
+	for _, tt := range tests {
+		t.Run(strconv.Itoa(tt.n), func(t *testing.T) {
+			if got := countPrimes(tt.n); got != tt.want {
+				t.Errorf("countPrimes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
