12638d34d48ed0a8ceef388475eef7617e1783bd
yidane <yidane@163.com>
2019-02-13 16:04:26 +0800

    342.power-of-four_1

--- leetcode/problems/400/342.power-of-four_1.go
+++ leetcode/problems/400/342.power-of-four_1.go
@@ -1,1 +1,15 @@
 package problems
+
+func isPowerOfFour(num int) bool {
+	if num == 1 || num == 4 {
+		return true
+	}
+
+	b := 4
+
+	for b < num {
+		b = b * 4
+	}
+
+	return b == num
+}
--- leetcode/problems/400/342.power-of-four_1_test.go
+++ leetcode/problems/400/342.power-of-four_1_test.go
@@ -1,1 +1,22 @@
 package problems
+
+import "testing"
+
+func Test_isPowerOfFour(t *testing.T) {
+	type args struct {
+		num  int
+		want bool
+	}
+	tests := []args{
+		{num: 16, want: true},
+		{num: 17, want: false},
+		{num: 1, want: true},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := isPowerOfFour(tt.num); got != tt.want {
+				t.Errorf("isPowerOfFour() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
