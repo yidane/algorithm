90b525b0ed33d70828e8b7fff21161aac68f1b94
yidane <yidane@163.com>
2019-02-13 16:34:26 +0800

    541.reverse-string-ii_1

--- leetcode/problems/600/541.reverse-string-ii_1.go
+++ leetcode/problems/600/541.reverse-string-ii_1.go
@@ -1,1 +1,24 @@
 package problems
+
+func reverseStr(s string, k int) string {
+	if len(s) <= 1 || k <= 1 {
+		return s
+	}
+
+	rs := []byte(s)
+
+	for i := 0; i < len(rs); i = i + 2*k {
+		m, n := i, i+k-1
+		if i+k > len(rs) {
+			n = len(rs) - 1
+		}
+
+		for m < n {
+			rs[m], rs[n] = rs[n], rs[m]
+			m++
+			n--
+		}
+	}
+
+	return string(rs)
+}
--- leetcode/problems/600/541.reverse-string-ii_1_test.go
+++ leetcode/problems/600/541.reverse-string-ii_1_test.go
@@ -1,1 +1,21 @@
 package problems
+
+import "testing"
+
+func Test_reverseStr(t *testing.T) {
+	type args struct {
+		s    string
+		k    int
+		want string
+	}
+	tests := []args{
+		{s: "abcdefg", k: 2, want: "bacdfeg"},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := reverseStr(tt.s, tt.k); got != tt.want {
+				t.Errorf("reverseStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
