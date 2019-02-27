24d3e95a848796fe6bfa87ce092569c58f799e6f
yidane <yidane@163.com>
2019-02-27 16:42:42 +0800

    update 177,180-185,196,197

---
+++ leetcode/problems/200/180.consecutive-numbers_2.sql
@@ -0,0 +1,45 @@
+--
+-- @lc app=leetcode id=180 lang=mysql
+--
+-- [180] Consecutive Numbers
+--
+-- https://leetcode.com/problems/consecutive-numbers/description/
+--
+-- database
+-- Medium (32.00%)
+-- Total Accepted:    46.4K
+-- Total Submissions: 145.1K
+-- Testcase Example:  '{"headers": {"Logs": ["Id", "Num"]}, "rows": {"Logs": [[1, 1], [2, 1], [3, 1], [4, 2], [5, 1], [6, 2], [7, 2]]}}'
+--
+-- Write a SQL query to find all numbers that appear at least three times
+-- consecutively.
+-- 
+-- 
+-- +----+-----+
+-- | Id | Num |
+-- +----+-----+
+-- | 1  |  1  |
+-- | 2  |  1  |
+-- | 3  |  1  |
+-- | 4  |  2  |
+-- | 5  |  1  |
+-- | 6  |  2  |
+-- | 7  |  2  |
+-- +----+-----+
+-- 
+-- 
+-- For example, given the above Logs table, 1 is the only number that appears
+-- consecutively for at least three times.
+-- 
+-- 
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+-- 
+-- 
+--
+# Write your MySQL query statement below
+
+
---
+++ leetcode/problems/200/182.duplicate-emails_1.sql
@@ -0,0 +1,41 @@
+--
+-- @lc app=leetcode id=182 lang=mysql
+--
+-- [182] Duplicate Emails
+--
+-- https://leetcode.com/problems/duplicate-emails/description/
+--
+-- database
+-- Easy (52.35%)
+-- Total Accepted:    99.1K
+-- Total Submissions: 189.2K
+-- Testcase Example:  '{"headers": {"Person": ["Id", "Email"]}, "rows": {"Person": [[1, "a@b.com"], [2, "c@d.com"], [3, "a@b.com"]]}}'
+--
+-- Write a SQL query to find all duplicate emails in a table named Person.
+-- 
+-- 
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- 
+-- 
+-- For example, your query should return the following for the above table:
+-- 
+-- 
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- 
+-- 
+-- Note: All emails are in lowercase.
+-- 
+--
+# Write your MySQL query statement below
+
+
