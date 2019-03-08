1e8290c221affbff36997fe5143199694fa3738b
yidane <yidane@163.com>
2019-03-08 16:52:01 +0800

    update 169 majorityElement

--- leetcode/problems/200/169.majority-element_1.go
+++ leetcode/problems/200/169.majority-element_1.go
@@ -1,1 +1,51 @@
 package problems
+
+/*
+ * @lc app=leetcode id=169 lang=golang
+ *
+ * [169] Majority Element
+ *
+ * https://leetcode.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (51.44%)
+ * Total Accepted:    351.5K
+ * Total Submissions: 681.4K
+ * Testcase Example:  '[3,2,3]'
+ *
+ * Given an array of size n, find the majority element. The majority element is
+ * the element that appears more than ⌊ n/2 ⌋ times.
+ *
+ * You may assume that the array is non-empty and the majority element always
+ * exist in the array.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [3,2,3]
+ * Output: 3
+ *
+ * Example 2:
+ *
+ *
+ * Input: [2,2,1,1,1,2,2]
+ * Output: 2
+ *
+ *
+ */
+func majorityElement(nums []int) int {
+	var m = make(map[int]int)
+	maj := len(nums) / 2
+
+	for _, i := range nums {
+		v := m[i]
+		v++
+		if v > maj {
+			return i
+		}
+
+		m[i] = v
+	}
+
+	return -1
+}
--- leetcode/problems/200/169.majority-element_1_test.go
+++ leetcode/problems/200/169.majority-element_1_test.go
@@ -1,1 +1,23 @@
 package problems
+
+import (
+	"strconv"
+	"testing"
+)
+
+func Test_majorityElement(t *testing.T) {
+	tests := []struct {
+		args []int
+		want int
+	}{
+		//{args: []int{3, 2, 3}, want: 3},
+		{args: []int{2, 2, 1, 1, 1, 2, 2}, want: 2},
+	}
+	for _, tt := range tests {
+		t.Run(strconv.Itoa(tt.want), func(t *testing.T) {
+			if got := majorityElement(tt.args); got != tt.want {
+				t.Errorf("majorityElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
