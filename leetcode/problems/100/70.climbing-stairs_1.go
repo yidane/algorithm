7139e2d3854cda61f9b7de23cc886ee90edc69f5
yidane <yidane@163.com>
2019-03-08 16:36:27 +0800

    update 70 climbStairs

--- leetcode/problems/100/70.climbing-stairs_1.go
+++ leetcode/problems/100/70.climbing-stairs_1.go
@@ -1,1 +1,70 @@
 package problems
+
+/*
+ * @lc app=leetcode id=70 lang=golang
+ *
+ * [70] Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (43.37%)
+ * Total Accepted:    360.9K
+ * Total Submissions: 829.5K
+ * Testcase Example:  '2'
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+ * you climb to the top?
+ *
+ * Note: Given n will be a positive integer.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ */
+
+/*
+   1 1
+   2 2
+   3 3
+   4 5
+*/
+func climbStairs(n int) int {
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	case 2:
+		return 2
+	default:
+		i1, i2, i3 := 1, 2, 3
+		for i := 3; i <= n; i++ {
+			i3 = i1 + i2
+			i1 = i2
+			i2 = i3
+		}
+
+		return i3
+	}
+}
--- leetcode/problems/100/70.climbing-stairs_1_test.go
+++ leetcode/problems/100/70.climbing-stairs_1_test.go
@@ -1,1 +1,25 @@
 package problems
+
+import (
+	"strconv"
+	"testing"
+)
+
+func Test_climbStairs(t *testing.T) {
+	tests := []struct {
+		arg  int
+		want int
+	}{
+		{arg: 1, want: 1},
+		{arg: 2, want: 2},
+		{arg: 3, want: 3},
+		{arg: 4, want: 5},
+	}
+	for _, tt := range tests {
+		t.Run(strconv.Itoa(tt.arg), func(t *testing.T) {
+			if got := climbStairs(tt.arg); got != tt.want {
+				t.Errorf("climbStairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
