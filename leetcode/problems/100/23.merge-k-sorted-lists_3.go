477ae6164daf750c40e0e9efa4350e6256e14854
yidane <yidane@163.com>
2019-03-11 16:59:31 +0800

    update 23

--- leetcode/problems/100/23.merge-k-sorted-lists_3.go
+++ leetcode/problems/100/23.merge-k-sorted-lists_3.go
@@ -1,1 +1,117 @@
 package problems
+
+import (
+	"sort"
+)
+
+/*
+ * @lc app=leetcode id=23 lang=golang
+ *
+ * [23] Merge k Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (33.14%)
+ * Total Accepted:    347.5K
+ * Total Submissions: 1M
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and
+ * describe its complexity.
+ *
+ * Example:
+ *
+ *
+ * Input:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+//节约内存
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 1 {
+		return lists[0]
+	}
+
+	head := new(ListNode)
+	var result = head
+	var cur *ListNode
+	complete := false
+	t := 0
+	for {
+		complete = true
+		for i := 0; i < len(lists); i++ {
+			if lists[i] == nil {
+				continue
+			}
+
+			complete = false
+
+			if cur == nil {
+				cur = lists[i]
+				t = i
+				continue
+			}
+
+			if lists[i].Val < cur.Val {
+				cur = lists[i]
+				t = i
+			}
+		}
+
+		head.Next = cur
+		head = head.Next
+
+		if complete {
+			break
+		}
+
+		cur = cur.Next
+
+		if lists[t] != nil {
+			lists[t] = lists[t].Next
+		}
+	}
+
+	return result.Next
+}
+
+func mergeKLists1(lists []*ListNode) *ListNode {
+	nums := make([]int, 0)
+
+	for i := 0; i < len(lists); i++ {
+		node := lists[i]
+		for node != nil {
+			nums = append(nums, node.Val)
+			node = node.Next
+		}
+	}
+
+	sort.Ints(nums)
+
+	var result = new(ListNode)
+	var head = result
+	for i := 0; i < len(nums); i++ {
+		head.Next = &ListNode{
+			Val: nums[i],
+		}
+
+		head = head.Next
+	}
+
+	return result.Next
+}
--- leetcode/problems/100/23.merge-k-sorted-lists_3_test.go
+++ leetcode/problems/100/23.merge-k-sorted-lists_3_test.go
@@ -1,1 +1,51 @@
 package problems
+
+import (
+	"testing"
+)
+
+func Test_mergeKLists(t *testing.T) {
+	tests := []struct {
+		args [][]int
+		want []int
+	}{
+		{args: [][]int{
+			{1, 4, 5},
+			{1, 3, 4},
+			{2, 6},
+		}, want: []int{1, 1, 2, 3, 4, 4, 5, 6}},
+		{args: [][]int{
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8}}, want: []int{1, 2, 3, 4, 5, 6, 7, 8}},
+		{args: [][]int{
+			{1, 2, 3},
+		}, want: []int{1, 2, 3}},
+		{args: [][]int{
+			{1}, {2}, {3},
+		}, want: []int{1, 2, 3}},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			var lists []*ListNode
+
+			for i := 0; i < len(tt.args); i++ {
+				lists = append(lists, NewListNode(tt.args[i]))
+			}
+
+			if got := mergeKLists(lists); !got.Equal(tt.want) {
+				t.Errorf("mergeKLists() = %v, want %v", got, tt.want)
+			}
+
+			var lists1 []*ListNode
+
+			for i := 0; i < len(tt.args); i++ {
+				lists1 = append(lists1, NewListNode(tt.args[i]))
+			}
+
+			if got := mergeKLists1(lists1); !got.Equal(tt.want) {
+				t.Errorf("mergeKLists1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
