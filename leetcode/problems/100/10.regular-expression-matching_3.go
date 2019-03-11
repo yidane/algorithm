1d3b021ab5f359b8ccf9b4465c8bd9c6e7382c74
yidane <yidane@163.com>
2019-03-11 13:25:58 +0800

    update 10 and add todo

--- leetcode/problems/100/10.regular-expression-matching_3.go
+++ leetcode/problems/100/10.regular-expression-matching_3.go
@@ -1,1 +1,117 @@
 package problems
+
+/*
+ * @lc app=leetcode id=10 lang=golang
+ *
+ * [10] Regular Expression Matching
+ *
+ * https://leetcode.com/problems/regular-expression-matching/description/
+ *
+ * algorithms
+ * Hard (25.00%)
+ * Total Accepted:    280.1K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * Given an input string (s) and a pattern (p), implement regular expression
+ * matching with support for '.' and '*'.
+ *
+ *
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ *
+ *
+ * The matching should cover the entire input string (not partial).
+ *
+ * Note:
+ *
+ *
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters
+ * like . or *.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the precedeng element, 'a'.
+ * Therefore, by repeating 'a' once, it becomes "aa".
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore
+ * it matches "aab".
+ *
+ *
+ * Example 5:
+ *
+ *
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ *
+ *
+ */
+
+//todo 不好解
+func isMatch(s string, p string) bool {
+	//if len(s) == 0 {
+	//	if p == "*" || p == "" {
+	//		return true
+	//	} else {
+	//		return false
+	//	}
+	//}
+	//
+	//if len(p) == 0 {
+	//	return false
+	//}
+	//
+	//var preceding uint8
+	//j := 0
+	//for i := 0; i < len(s); i++ {
+	//	r := s[i]
+	//	pr := p[j]
+	//
+	//	if r == pr || pr == '.' || r == preceding {
+	//		return false
+	//	}
+	//
+	//	if pr != '*' {
+	//		j++
+	//	}
+	//}
+
+	return true
+}
--- leetcode/problems/100/10.regular-expression-matching_3_test.go
+++ leetcode/problems/100/10.regular-expression-matching_3_test.go
@@ -1,1 +1,24 @@
 package problems
+
+import "testing"
+
+func Test_isMatch(t *testing.T) {
+	tests := []struct {
+		s    string
+		p    string
+		want bool
+	}{
+		{s: "aa", p: "a", want: false},
+		{s: "aa", p: "a*", want: true},
+		{s: "ab", p: ".*", want: true},
+		{s: "aab", p: "c*a*b", want: true},
+		{s: "mississippi", p: "mis*is*p*.", want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.s, func(t *testing.T) {
+			if got := isMatch(tt.s, tt.p); got != tt.want {
+				t.Errorf("isMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
