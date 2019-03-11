9c9a0d51ec3bb3feec6a6c57c2d7d0cc27fe8dab
yidane <yidane@163.com>
2019-03-11 11:13:46 +0800

    update 258

--- leetcode/problems/300/258.add-digits_1.go
+++ leetcode/problems/300/258.add-digits_1.go
@@ -1,1 +1,42 @@
 package problems
+
+/*
+ * @lc app=leetcode id=258 lang=golang
+ *
+ * [258] Add Digits
+ *
+ * https://leetcode.com/problems/add-digits/description/
+ *
+ * algorithms
+ * Easy (53.58%)
+ * Total Accepted:    228.6K
+ * Total Submissions: 426.7K
+ * Testcase Example:  '38'
+ *
+ * Given a non-negative integer num, repeatedly add all its digits until the
+ * result has only one digit.
+ *
+ * Example:
+ *
+ *
+ * Input: 38
+ * Output: 2
+ * Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
+ * Since 2 has only one digit, return it.
+ *
+ *
+ * Follow up:
+ * Could you do it without any loop/recursion in O(1) runtime?
+ */
+func addDigits(num int) int {
+	for num > 9 {
+		n := 0
+		for num > 0 {
+			n += num % 10
+			num /= 10
+		}
+		num = n
+	}
+
+	return num
+}
--- leetcode/problems/300/258.add-digits_1_test.go
+++ leetcode/problems/300/258.add-digits_1_test.go
@@ -1,1 +1,22 @@
 package problems
+
+import (
+	"strconv"
+	"testing"
+)
+
+func Test_addDigits(t *testing.T) {
+	tests := []struct {
+		args int
+		want int
+	}{
+		{args: 38, want: 2},
+	}
+	for _, tt := range tests {
+		t.Run(strconv.Itoa(tt.args), func(t *testing.T) {
+			if got := addDigits(tt.args); got != tt.want {
+				t.Errorf("addDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
