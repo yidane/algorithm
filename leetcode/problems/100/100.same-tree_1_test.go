dc4e95692221c40ca1b283912f426dff18b0755f
yidane <yidane@163.com>
2019-03-12 18:14:04 +0800

    update 100

--- leetcode/problems/100/100.same-tree_1_test.go
+++ leetcode/problems/100/100.same-tree_1_test.go
@@ -1,1 +1,24 @@
 package problems
+
+import "testing"
+
+func Test_isSameTree(t *testing.T) {
+	type args struct {
+		p *TreeNode
+		q *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSameTree(tt.args.p, tt.args.q); got != tt.want {
+				t.Errorf("isSameTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
