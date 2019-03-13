3f065e3c2fd014eaa113fe12d61df56a52151769
yidane <yidane@163.com>
2019-03-13 09:51:18 +0800

    update 965

--- leetcode/problems/965.unique-email-addresses_1.go
+++ leetcode/problems/965.unique-email-addresses_1.go
@@ -1,1 +1,74 @@
 package problems
+
+/*
+ * @lc app=leetcode id=965 lang=golang
+ *
+ * [965] Univalued Binary Tree
+ *
+ * https://leetcode.com/problems/univalued-binary-tree/description/
+ *
+ * algorithms
+ * Easy (67.52%)
+ * Total Accepted:    22K
+ * Total Submissions: 32.6K
+ * Testcase Example:  '[1,1,1,1,1,null,1]'
+ *
+ * A binary tree is univalued if every node in the tree has the same value.
+ *
+ * Return true if and only if the given tree is univalued.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,1,1,1,1,null,1]
+ * Output: true
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [2,2,2,5,2]
+ * Output: false
+ *
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * The number of nodes in the given tree will be in the range [1, 100].
+ * Each node's value will be an integer in the range [0, 99].
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isUnivalTree(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return isUnivalNode(root.Left, root.Val) && isUnivalNode(root.Right, root.Val)
+}
+
+func isUnivalNode(node *TreeNode, val int) bool {
+	if node == nil {
+		return true
+	}
+
+	if node.Val != val {
+		return false
+	}
+
+	return isUnivalNode(node.Left, val) && isUnivalNode(node.Right, val)
+}
