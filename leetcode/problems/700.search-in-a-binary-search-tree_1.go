5ee36007dd4fddcb054bed9b7968a2ea9cfe1161
yidane <yidane@163.com>
2019-03-13 09:43:10 +0800

    update 700

--- leetcode/problems/700.search-in-a-binary-search-tree_1.go
+++ leetcode/problems/700.search-in-a-binary-search-tree_1.go
@@ -55,5 +55,17 @@ package problems
  * }
  */
 func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
 
+	if root.Val == val {
+		return root
+	}
+
+	if root.Val < val {
+		return searchBST(root.Right, val)
+	} else {
+		return searchBST(root.Left, val)
+	}
 }
