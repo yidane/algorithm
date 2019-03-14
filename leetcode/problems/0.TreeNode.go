3c890226628e5ab75b98f05bdc5b980b8d36b66b
yidane <yidane@163.com>
2019-03-14 15:53:38 +0800

    update 669

--- leetcode/problems/0.TreeNode.go
+++ leetcode/problems/0.TreeNode.go
@@ -25,6 +25,25 @@ func (t *TreeNode) AddRight(v int) *TreeNode {
 	return t
 }
 
+func (root *TreeNode) Equal(root1 *TreeNode) bool {
+	if root == nil {
+		if root1 == nil {
+			return true
+		}
+		return false
+	} else {
+		if root1 == nil {
+			return false
+		}
+	}
+
+	if root.Val != root1.Val {
+		return false
+	}
+
+	return root.Left.Equal(root1.Left) && root.Right.Equal(root1.Right)
+}
+
 //func NewTreeNode(arr []int) *TreeNode {
 //	if arr == nil || len(arr) == 0 {
 //		return nil
--- leetcode/problems/669.trim-a-binary-search-tree_1_test.go
+++ leetcode/problems/669.trim-a-binary-search-tree_1_test.go
@@ -1,1 +1,33 @@
 package problems
+
+import (
+	"testing"
+)
+
+func Test_trimBST(t *testing.T) {
+	tests := []struct {
+		root func() *TreeNode
+		L    int
+		R    int
+		want func() *TreeNode
+	}{
+		{root: func() *TreeNode {
+			return NewTreeNode(1).AddLeft(0).AddRight(2)
+		}, L: 1, R: 2, want: func() *TreeNode {
+			return NewTreeNode(1).AddRight(2)
+		}},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := trimBST(tt.root(), tt.L, tt.R); !got.Equal(tt.want()) {
+				t.Fail()
+			}
+		})
+
+		t.Run("", func(t *testing.T) {
+			if got := trimBST1(tt.root(), tt.L, tt.R); !got.Equal(tt.want()) {
+				t.Fail()
+			}
+		})
+	}
+}
