51ba2c69dcce9877eab8a4f11464156807ed0cb4
yidane <yidane@163.com>
2019-03-18 14:27:17 +0800

    update 122

--- leetcode/problems/122.best-time-to-buy-and-sell-stock-ii_1_test.go
+++ leetcode/problems/122.best-time-to-buy-and-sell-stock-ii_1_test.go
@@ -1,1 +1,24 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_maxProfit(t *testing.T) {
+	tests := []struct {
+		prices []int
+		want   int
+	}{
+		{prices: []int{7, 1, 5, 3, 6, 4}, want: 7},
+		{prices: []int{1, 2, 3, 4, 5}, want: 4},
+		{prices: []int{7, 6, 4, 3, 1}, want: 0},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.prices), func(t *testing.T) {
+			if got := maxProfit(tt.prices); got != tt.want {
+				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
