ccc9653214be084021fce8955171b2c71720df83
yidane <yidane@163.com>
2019-03-18 15:46:25 +0800

    update 455

--- leetcode/problems/455.assign-cookies_1_test.go
+++ leetcode/problems/455.assign-cookies_1_test.go
@@ -1,1 +1,24 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_findContentChildren(t *testing.T) {
+	tests := []struct {
+		g    []int
+		s    []int
+		want int
+	}{
+		//{[]int{1, 2, 3}, []int{1, 1}, 1},
+		{[]int{1, 2}, []int{1, 2, 3}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.g, tt.s), func(t *testing.T) {
+			if got := findContentChildren(tt.g, tt.s); got != tt.want {
+				t.Errorf("findContentChildren() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
