cfcf56c744e6df84cc278397b53c7f25e2f14205
yidane <yidane@163.com>
2019-03-22 16:31:59 +0800

    update 43 but not the best

--- leetcode/problems/43.multiply-strings_2.go
+++ leetcode/problems/43.multiply-strings_2.go
@@ -1,5 +1,9 @@
 package problems
 
+import (
+	"bytes"
+)
+
 /*
  * @lc app=leetcode id=43 lang=golang
  *
@@ -41,7 +45,60 @@ package problems
  *
  *
  */
+/*
+		  1  2  3
+		* 4  5  6
+		  6  12	18
+	   5  10 15
+  + 4  8  12
+	5  6  0  8  8
+
+len(m)+len(n)-1  [len(m)][len(m)+len(n)-1]arr
+*/
+
+//todo not the best
 func multiply(num1 string, num2 string) string {
-	//todo
-	return ""
+	width := len(num1) + len(num2) - 1
+	arr := make(map[int]map[int]int, width)
+
+	for i := 0; i < len(num1); i++ {
+		subArr := make(map[int]int)
+		for j := 0; j < len(num2); j++ {
+			subArr[i+j] = int(num1[i]-48) * int(num2[j]-48)
+		}
+		arr[i] = subArr
+	}
+
+	addBit := 0
+
+	arr1 := make(map[int]int, width)
+	for i := width - 1; i >= 0; i-- {
+		t := 0
+		for j := 0; j < len(arr); j++ {
+			t += arr[j][i]
+		}
+		t += addBit
+
+		addBit = t / 10
+		arr1[i] = t % 10
+	}
+
+	buf := bytes.Buffer{}
+	if addBit > 0 {
+		buf.WriteRune(rune(addBit + 48))
+	}
+
+	for i := 0; i < len(arr1); i++ {
+		r := arr1[i]
+		if buf.Len() == 0 && r == 0 {
+			continue
+		}
+		buf.WriteRune(rune(r + 48))
+	}
+
+	if buf.Len() == 0 {
+		return "0"
+	}
+
+	return buf.String()
 }
--- leetcode/problems/43.multiply-strings_2_test.go
+++ leetcode/problems/43.multiply-strings_2_test.go
@@ -8,10 +8,11 @@ func Test_multiply(t *testing.T) {
 		num2 string
 		want string
 	}{
-		//{num1: "0", num2: "0", want: "0"},
-		//{num1: "2", num2: "3", want: "6"},
-		//{num1: "408", num2: "5", want: "2040"},
-		//{num1: "123", num2: "456", want: "56088"},
+		{num1: "0", num2: "0", want: "0"},
+		{num1: "2", num2: "3", want: "6"},
+		{num1: "408", num2: "5", want: "2040"},
+		{num1: "123", num2: "456", want: "56088"},
+		{num1: "9123", num2: "0", want: "0"},
 		{num1: "498828660196", num2: "840477629533", want: "419254329864656431168468"},
 	}
 	for _, tt := range tests {
