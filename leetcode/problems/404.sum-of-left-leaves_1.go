a15eff0bedce43c24467a53baa5452d651990069
yidane <yidane@163.com>
2019-03-26 15:05:09 +0800

    update 404

--- leetcode/problems/404.sum-of-left-leaves_1.go
+++ leetcode/problems/404.sum-of-left-leaves_1.go
@@ -1,1 +1,57 @@
 package problems
+
+/*
+ * @lc app=leetcode id=404 lang=golang
+ *
+ * [404] Sum of Left Leaves
+ *
+ * https://leetcode.com/problems/sum-of-left-leaves/description/
+ *
+ * algorithms
+ * Easy (48.74%)
+ * Total Accepted:    119K
+ * Total Submissions: 244.1K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Find the sum of all left leaves in a given binary tree.
+ *
+ * Example:
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * There are two left leaves in the binary tree, with values 9 and 15
+ * respectively. Return 24.
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumOfLeftLeaves(root *TreeNode) int {
+	return sumLeft(root, false)
+}
+
+func sumLeft(node *TreeNode, contain bool) int {
+	if node == nil {
+		return 0
+	}
+
+	sum := 0
+	if contain && node.Left == nil && node.Right == nil {
+		sum += node.Val
+	}
+
+	sum += sumLeft(node.Left, true)
+	sum += sumLeft(node.Right, false)
+
+	return sum
+}
--- leetcode/problems/404.sum-of-left-leaves_1_test.go
+++ leetcode/problems/404.sum-of-left-leaves_1_test.go
@@ -1,1 +1,25 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_sumOfLeftLeaves(t *testing.T) {
+	tests := []struct {
+		args []interface{}
+		want int
+	}{
+		{args: []interface{}{3, 9, 20, nil, nil, 15, 7}, want: 24},
+		{args: []interface{}{1, 2, 3, 4, 5}, want: 4},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			root := NewTree(tt.args)
+
+			if got := sumOfLeftLeaves(root); got != tt.want {
+				t.Errorf("sumOfLeftLeaves() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
