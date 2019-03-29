0c8303cfbc1184c72552b0bb5ffe6c6ef0ab075a
yidane <yidane@163.com>
2019-03-29 17:34:01 +0800

    update 168

--- leetcode/problems/168.excel-sheet-column-title_1.go
+++ leetcode/problems/168.excel-sheet-column-title_1.go
@@ -1,1 +1,78 @@
 package problems
+
+import (
+	"bytes"
+)
+
+/*
+ * @lc app=leetcode id=168 lang=golang
+ *
+ * [168] Excel Sheet Column Title
+ *
+ * https://leetcode.com/problems/excel-sheet-column-title/description/
+ *
+ * algorithms
+ * Easy (28.65%)
+ * Total Accepted:    166.2K
+ * Total Submissions: 580.3K
+ * Testcase Example:  '1'
+ *
+ * Given a positive integer, return its corresponding column title as appear in
+ * an Excel sheet.
+ *
+ * For example:
+ *
+ *
+ * ⁠   1 -> A
+ * ⁠   2 -> B
+ * ⁠   3 -> C
+ * ⁠   ...
+ * ⁠   26 -> Z
+ * ⁠   27 -> AA
+ * ⁠   28 -> AB
+ * ⁠   ...
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: 1
+ * Output: "A"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 28
+ * Output: "AB"
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: 701
+ * Output: "ZY"
+ *
+ */
+func convertToTitle(n int) string {
+	var rs []rune
+
+	for n > 26 {
+		d := n % 26
+		if d == 0 { //要考虑到刚好是26的整数倍，52=AZ...
+			rs = append(rs, 'Z')
+			n = (n - 1) / 26
+		} else {
+			rs = append(rs, rune(d+64))
+			n = n / 26
+		}
+	}
+	rs = append(rs, rune(n+64))
+
+	buf := bytes.Buffer{}
+	for i := len(rs) - 1; i >= 0; i-- {
+		buf.WriteRune(rs[i])
+	}
+
+	return buf.String()
+}
--- leetcode/problems/168.excel-sheet-column-title_1_test.go
+++ leetcode/problems/168.excel-sheet-column-title_1_test.go
@@ -1,1 +1,27 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_convertToTitle(t *testing.T) {
+	tests := []struct {
+		args int
+		want string
+	}{
+		//{1, "A"},
+		//{28, "AB"},
+		{52, "AZ"},
+		//{701, "ZY"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.want, func(t *testing.T) {
+			if got := convertToTitle(tt.args); got != tt.want {
+				t.Errorf("convertToTitle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+	fmt.Println('A')
+}
