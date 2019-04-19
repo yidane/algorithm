7d386771ade62077ce2b55705ea20c062939d30a
yidane <yidane@163.com>
2019-04-19 15:07:34 +0800

    update 155

--- leetcode/problems/155.min-stack_1.go
+++ leetcode/problems/155.min-stack_1.go
@@ -1,1 +1,95 @@
 package problems
+
+/*
+ * @lc app=leetcode id=155 lang=golang
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (36.43%)
+ * Total Accepted:    287.1K
+ * Total Submissions: 787.8K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ *
+ *
+ * push(x) -- Push element x onto stack.
+ *
+ *
+ * pop() -- Removes the element on top of the stack.
+ *
+ *
+ * top() -- Get the top element.
+ *
+ *
+ * getMin() -- Retrieve the minimum element in the stack.
+ *
+ *
+ *
+ *
+ * Example:
+ *
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ *
+ *
+ */
+type MinStack struct {
+	data    []int
+	minData []int
+}
+
+/** initialize your data structure here. */
+func NewMinStack() MinStack {
+	return MinStack{}
+}
+
+func (minStack *MinStack) Push(x int) {
+	minStack.data = append(minStack.data, x)
+
+	if len(minStack.minData) == 0 {
+		minStack.minData = append(minStack.minData, x)
+		return
+	}
+
+	minData := minStack.GetMin()
+	if x < minData {
+		minStack.minData = append(minStack.minData, x)
+	} else {
+		minStack.minData = append(minStack.minData, minData)
+	}
+}
+
+func (minStack *MinStack) Pop() {
+	l := len(minStack.minData) - 1
+	minStack.data = minStack.data[:l]
+	minStack.minData = minStack.minData[0:l]
+}
+
+func (minStack *MinStack) Top() int {
+	return minStack.data[len(minStack.data)-1]
+}
+
+func (minStack *MinStack) GetMin() int {
+	return minStack.minData[len(minStack.minData)-1]
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
--- leetcode/problems/155.min-stack_1_test.go
+++ leetcode/problems/155.min-stack_1_test.go
@@ -1,1 +1,46 @@
 package problems
+
+import (
+	"testing"
+)
+
+func TestNewMinStack(t *testing.T) {
+	minStack := NewMinStack()
+	minStack.Push(-2)
+	minStack.Push(0)
+	minStack.Push(-3)
+	if minStack.GetMin() != -3 {
+		t.Fatal()
+	}
+
+	minStack.Pop()
+	if minStack.Top() != 0 {
+		t.Fatal()
+	}
+
+	if minStack.GetMin() != -2 {
+		t.Fatal()
+	}
+
+	//["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
+	//[[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
+
+	ms := NewMinStack()
+	ms.Push(2147483646)
+	ms.Push(2147483646)
+	ms.Push(2147483646)
+	ms.Top()
+	ms.Pop()
+	ms.GetMin()
+	ms.Top()
+	ms.GetMin()
+	ms.Pop()
+	ms.Push(2147483647)
+	ms.Top()
+	ms.GetMin()
+	ms.Push(-2147483648)
+	ms.Top()
+	ms.GetMin()
+	ms.Pop()
+	ms.GetMin()
+}
