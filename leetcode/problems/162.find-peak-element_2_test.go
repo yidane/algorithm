9c66f3204fe44929b5927c1ca1d0d495c6e17ec3
yidane <yidane@163.com>
2019-04-19 16:08:55 +0800

    update 162

--- leetcode/problems/162.find-peak-element_2_test.go
+++ leetcode/problems/162.find-peak-element_2_test.go
@@ -1,1 +1,27 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_findPeakElement(t *testing.T) {
+	tests := []struct {
+		args []int
+		want int
+	}{
+		{[]int{1}, 0},
+		{[]int{1, 2}, 1},
+		{[]int{1, 2, 1}, 1},
+		{[]int{1, 2, 3}, 2},
+		{[]int{1, 2, 3, 2}, 2},
+		{[]int{1, 2, 3, 4}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			if got := findPeakElement(tt.args); got != tt.want {
+				t.Errorf("findPeakElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
