861a4b62b2d426c66f946b642d99bbaa0d186599
yidane <yidane@163.com>
2019-04-20 23:26:33 +0800

    update 33

--- leetcode/problems/33.search-in-rotated-sorted-array_2.go
+++ leetcode/problems/33.search-in-rotated-sorted-array_2.go
@@ -1,1 +1,67 @@
 package problems
+
+/*
+ * @lc app=leetcode id=33 lang=golang
+ *
+ * [33] Search in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (32.78%)
+ * Total Accepted:    396.1K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ *
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ *
+ * You are given a target value to search. If found in the array return its
+ * index, otherwise return -1.
+ *
+ * You may assume no duplicate exists in the array.
+ *
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ *
+ */
+func search33(nums []int, target int) int {
+	for i, j := 0, len(nums)-1; i <= j; {
+		left := nums[i]
+		right := nums[j]
+
+		if left == target {
+			return i
+		}
+
+		if right == target {
+			return j
+		}
+
+		if left > target {
+			if right < target {
+				return -1
+			} else {
+				j--
+			}
+		} else {
+			i++
+		}
+	}
+
+	return -1
+}
--- leetcode/problems/33.search-in-rotated-sorted-array_2_test.go
+++ leetcode/problems/33.search-in-rotated-sorted-array_2_test.go
@@ -1,1 +1,26 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_search(t *testing.T) {
+	tests := []struct {
+		nums   []int
+		target int
+		want   int
+	}{
+		{[]int{4, 5, 6, 7, 0, 1, 2}, 0, 4},
+		{[]int{4, 5, 6, 7, 0, 1, 2}, 5, 1},
+		{[]int{4, 5, 6, 7, 0, 1, 2}, 3, -1},
+		{[]int{1, 3, 5}, 3, 1},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.nums, tt.target), func(t *testing.T) {
+			if got := search33(tt.nums, tt.target); got != tt.want {
+				t.Errorf("search() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
