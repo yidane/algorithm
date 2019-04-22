4b99dae8727a4cf84df7277260145109f7dd26ee
yidane <yidane@163.com>
2019-04-22 15:33:55 +0800

    update 110

--- leetcode/problems/110.balanced-binary-tree_1.go
+++ leetcode/problems/110.balanced-binary-tree_1.go
@@ -1,1 +1,90 @@
 package problems
+
+/*
+ * @lc app=leetcode id=110 lang=golang
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (40.70%)
+ * Total Accepted:    310.4K
+ * Total Submissions: 762.3K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ *
+ * For this problem, a height-balanced binary tree is defined as:
+ *
+ *
+ * a binary tree in which the depth of the two subtrees of every node never
+ * differ by more than 1.
+ *
+ *
+ * Example 1:
+ *
+ * Given the following tree [3,9,20,null,null,15,7]:
+ *
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * Return true.
+ *
+ * Example 2:
+ *
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ *
+ *
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ *
+ *
+ * Return false.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+	return checkBalanced(root) != -1
+}
+
+func checkBalanced(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := checkBalanced(root.Left)
+	if left == -1 {
+		return -1
+	}
+
+	right := checkBalanced(root.Right)
+	if right == -1 {
+		return -1
+	}
+
+	if right < left {
+		left, right = right, left
+	}
+
+	if right-left > 1 {
+		return -1
+	}
+	return right + 1
+}
--- leetcode/problems/110.balanced-binary-tree_1_test.go
+++ leetcode/problems/110.balanced-binary-tree_1_test.go
@@ -1,1 +1,25 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_isBalanced(t *testing.T) {
+	tests := []struct {
+		args []interface{}
+		want bool
+	}{
+		{[]interface{}{3, 9, 20, nil, nil, 15, 7}, true},
+		{[]interface{}{1, 2, 2, 3, 3, nil, nil, 4, 4}, false},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			root := NewTree(tt.args)
+
+			if got := isBalanced(root); got != tt.want {
+				t.Errorf("isBalanced() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
