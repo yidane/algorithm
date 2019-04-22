3d4df85360e901d406ff86317e344f987108e565
yidane <yidane@163.com>
2019-04-22 14:22:25 +0800

    update 66

--- leetcode/problems/66.plus-one_1.go
+++ leetcode/problems/66.plus-one_1.go
@@ -1,1 +1,73 @@
 package problems
+
+/*
+ * @lc app=leetcode id=66 lang=golang
+ *
+ * [66] Plus One
+ *
+ * https://leetcode.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (40.99%)
+ * Total Accepted:    377.1K
+ * Total Submissions: 919.5K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a non-empty array of digits representing a non-negative integer, plus
+ * one to the integer.
+ *
+ * The digits are stored such that the most significant digit is at the head of
+ * the list, and each element in the array contain a single digit.
+ *
+ * You may assume the integer does not contain any leading zero, except the
+ * number 0 itself.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ *
+ */
+func plusOne(digits []int) []int {
+	b := 0
+
+	last := digits[len(digits)-1] + 1
+	if last == 10 {
+		last = 0
+		b = 1
+	}
+	digits[len(digits)-1] = last
+
+	for i := len(digits) - 2; i >= 0; i-- {
+		if b == 0 {
+			break
+		}
+		last = digits[i] + 1
+		if last == 10 {
+			digits[i] = 0
+			continue
+		}
+
+		digits[i] = last
+		b = 0
+		break
+	}
+
+	if b == 0 {
+		return digits
+	}
+
+	result := []int{b}
+	result = append(result, digits...)
+	return result
+}
--- leetcode/problems/66.plus-one_1_test.go
+++ leetcode/problems/66.plus-one_1_test.go
@@ -1,1 +1,29 @@
 package problems
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Test_plusOne(t *testing.T) {
+	tests := []struct {
+		args []int
+		want []int
+	}{
+		{[]int{1, 2, 3}, []int{1, 2, 4}},
+		{[]int{4, 3, 2, 1}, []int{4, 3, 2, 2}},
+		{[]int{8}, []int{9}},
+		{[]int{9}, []int{1, 0}},
+		{[]int{9, 9}, []int{1, 0, 0}},
+		{[]int{9, 9, 9}, []int{1, 0, 0, 0}},
+		{[]int{8, 9, 9, 9}, []int{9, 0, 0, 0}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			if got := plusOne(tt.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("plusOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
