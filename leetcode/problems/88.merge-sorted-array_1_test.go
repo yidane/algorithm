24d9fd73f372a3067cec1263f44c9a055ba7028c
yidane <yidane@163.com>
2019-04-22 13:57:46 +0800

    update 88

--- leetcode/problems/88.merge-sorted-array_1_test.go
+++ leetcode/problems/88.merge-sorted-array_1_test.go
@@ -1,1 +1,30 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	tests := []struct {
+		nums1 []int
+		m     int
+		nums2 []int
+		n     int
+		want  []int
+	}{
+		{[]int{0}, 0, []int{1}, 1, []int{1}},
+		{[]int{2, 0}, 1, []int{1}, 1, []int{1, 2}},
+		{[]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3, []int{1, 2, 2, 3, 5, 6}},
+		{[]int{1, 2, 4, 5, 6, 0}, 5, []int{3}, 1, []int{1, 2, 3, 4, 5, 6}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.nums1), func(t *testing.T) {
+			merge(tt.nums1, tt.m, tt.nums2, tt.n)
+
+			if !SameArray(tt.nums1, tt.want) {
+				t.Fatal(tt.nums1, tt.want)
+			}
+		})
+	}
+}
