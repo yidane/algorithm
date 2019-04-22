8ab63661646ef76219e69c249bb285497017bad4
yidane <yidane@163.com>
2019-04-22 16:19:18 +0800

    update 119

--- leetcode/problems/119.pascals-triangle-ii_1.go
+++ leetcode/problems/119.pascals-triangle-ii_1.go
@@ -1,1 +1,63 @@
 package problems
+
+/*
+ * @lc app=leetcode id=119 lang=golang
+ *
+ * [119] Pascal's Triangle II
+ *
+ * https://leetcode.com/problems/pascals-triangle-ii/description/
+ *
+ * algorithms
+ * Easy (42.90%)
+ * Total Accepted:    196.2K
+ * Total Submissions: 456.6K
+ * Testcase Example:  '3'
+ *
+ * Given a non-negative index k where k ≤ 33, return the k^th index row of the
+ * Pascal's triangle.
+ *
+ * Note that the row index starts from 0.
+ *
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it.
+ *
+ * Example:
+ *
+ *
+ * Input: 3
+ * Output: [1,3,3,1]
+ *
+ *
+ * Follow up:
+ *
+ * Could you optimize your algorithm to use only O(k) extra space?
+ *
+ */
+func getRow(rowIndex int) []int {
+	if rowIndex < 0 {
+		return nil
+	}
+
+	if rowIndex == 0 {
+		return []int{1}
+	}
+
+	if rowIndex == 1 {
+		return []int{1, 1}
+	}
+
+	result := []int{1, 1}
+	for i := 2; i <= rowIndex; i++ {
+		newArr := make([]int, len(result)+1)
+		newArr[0] = 1
+
+		for j := 0; j < len(result)-1; j++ {
+			newArr[j+1] = result[j] + result[j+1]
+		}
+		newArr[len(newArr)-1] = 1
+		result = newArr
+	}
+
+	return result
+}
--- leetcode/problems/119.pascals-triangle-ii_1_test.go
+++ leetcode/problems/119.pascals-triangle-ii_1_test.go
@@ -1,1 +1,20 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_getRow(t *testing.T) {
+	tests := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, tt := range tests {
+		pascals := generate(30)
+
+		t.Run(fmt.Sprint(tt), func(t *testing.T) {
+			got := getRow(tt)
+			if !SameArray(got, pascals[tt]) {
+				t.Fatal()
+			}
+		})
+	}
+}
