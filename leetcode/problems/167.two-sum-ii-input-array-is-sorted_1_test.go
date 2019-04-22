06ede99bd1e0a6a1403c4e12d57f70ae783d7f9b
yidane <yidane@163.com>
2019-04-22 17:45:51 +0800

    update 167

--- leetcode/problems/167.two-sum-ii-input-array-is-sorted_1_test.go
+++ leetcode/problems/167.two-sum-ii-input-array-is-sorted_1_test.go
@@ -1,1 +1,24 @@
 package problems
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Test_twoSum167(t *testing.T) {
+	tests := []struct {
+		numbers []int
+		target  int
+		want    []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{1, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.numbers), func(t *testing.T) {
+			if got := twoSum167(tt.numbers, tt.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
