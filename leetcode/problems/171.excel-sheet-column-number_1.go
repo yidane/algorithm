004a53ad3e06084e558fb83f1d8165e8dbe12bd4
yidane <yidane@163.com>
2019-04-22 17:56:57 +0800

    update 171

--- leetcode/problems/171.excel-sheet-column-number_1.go
+++ leetcode/problems/171.excel-sheet-column-number_1.go
@@ -1,1 +1,66 @@
 package problems
+
+/*
+ * @lc app=leetcode id=171 lang=golang
+ *
+ * [171] Excel Sheet Column Number
+ *
+ * https://leetcode.com/problems/excel-sheet-column-number/description/
+ *
+ * algorithms
+ * Easy (51.22%)
+ * Total Accepted:    214.6K
+ * Total Submissions: 418.7K
+ * Testcase Example:  '"A"'
+ *
+ * Given a column title as appear in an Excel sheet, return its corresponding
+ * column number.
+ *
+ * For example:
+ *
+ *
+ * ⁠   A -> 1
+ * ⁠   B -> 2
+ * ⁠   C -> 3
+ * ⁠   ...
+ * ⁠   Z -> 26
+ * ⁠   AA -> 27
+ * ⁠   AB -> 28
+ * ⁠   ...
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: "A"
+ * Output: 1
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: "AB"
+ * Output: 28
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: "ZY"
+ * Output: 701
+ *
+ */
+func titleToNumber(s string) int {
+	result := 0
+	l := len(s)
+	for i := l - 1; i >= 0; i-- {
+		r := int(s[l-1-i] - 64)
+		for j := i; j > 0; j-- {
+			r *= 26
+		}
+
+		result += r
+	}
+
+	return result
+}
--- leetcode/problems/171.excel-sheet-column-number_1_test.go
+++ leetcode/problems/171.excel-sheet-column-number_1_test.go
@@ -1,1 +1,26 @@
 package problems
+
+import (
+	"testing"
+)
+
+func Test_titleToNumber(t *testing.T) {
+	tests := []struct {
+		args string
+		want int
+	}{
+		{"A", 1},
+		{"B", 2},
+		{"Z", 26},
+		{"AA", 27},
+		{"AB", 28},
+		{"ZY", 701},
+	}
+	for _, tt := range tests {
+		t.Run(tt.args, func(t *testing.T) {
+			if got := titleToNumber(tt.args); got != tt.want {
+				t.Errorf("titleToNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
