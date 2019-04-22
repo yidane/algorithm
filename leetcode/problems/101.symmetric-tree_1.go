cfed63023eb3c43a96d6b134f236780b1d1847a5
yidane <yidane@163.com>
2019-04-22 14:10:32 +0800

    update 101

--- leetcode/problems/101.symmetric-tree_1.go
+++ leetcode/problems/101.symmetric-tree_1.go
@@ -1,1 +1,71 @@
 package problems
+
+/*
+ * @lc app=leetcode id=101 lang=golang
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (43.14%)
+ * Total Accepted:    385.3K
+ * Total Submissions: 892.4K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ *
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ *
+ *
+ *
+ * But the following [1,2,2,null,3,null,3]  is not:
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ *
+ *
+ *
+ *
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	return isSymentricFunc(root, root)
+}
+
+func isSymentricFunc(left *TreeNode, right *TreeNode) bool {
+	if left == nil {
+		return right == nil
+	}
+
+	if right == nil {
+		return false
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return isSymentricFunc(left.Left, right.Right) && isSymentricFunc(left.Right, right.Left)
+}
--- leetcode/problems/101.symmetric-tree_1_test.go
+++ leetcode/problems/101.symmetric-tree_1_test.go
@@ -1,1 +1,25 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_isSymmetric(t *testing.T) {
+	tests := []struct {
+		args []interface{}
+		want bool
+	}{
+		{[]interface{}{1, 2, 2, 3, 4, 4, 3}, true},
+		{[]interface{}{1, 2, 2, nil, 3, nil, 3}, false},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			root := NewTree(tt.args)
+
+			if got := isSymmetric(root); got != tt.want {
+				t.Errorf("isSymmetric() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
