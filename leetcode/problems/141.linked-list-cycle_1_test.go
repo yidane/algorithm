41ae0afb53f88c52f91db04687b3e7b772d1e724
yidane <yidane@163.com>
2019-04-22 17:39:32 +0800

    update 141 快慢指针

--- leetcode/problems/141.linked-list-cycle_1_test.go
+++ leetcode/problems/141.linked-list-cycle_1_test.go
@@ -1,1 +1,42 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_hasCycle(t *testing.T) {
+	tests := []struct {
+		args []int
+		pos  int
+		want bool
+	}{
+		{[]int{3, 2, 0, 4}, 1, true},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			head := NewListNode(tt.args)
+
+			tail := head
+			for tail.Next != nil {
+				tail = tail.Next
+			}
+
+			if tt.pos != -1 {
+				posNode := head
+				for tt.pos >= 0 {
+					if posNode.Next != nil {
+						posNode = posNode.Next
+					}
+					tt.pos--
+				}
+
+				tail.Next = posNode
+			}
+
+			if got := hasCycle(head); got != tt.want {
+				t.Errorf("hasCycle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
