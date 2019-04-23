e12ea93977e3a2936bdf9bd77f1a4fbf5a2f3d6d
yidane <yidane@163.com>
2019-04-23 14:44:51 +0800

    update 231

--- leetcode/problems/231.power-of-two_1_test.go
+++ leetcode/problems/231.power-of-two_1_test.go
@@ -1,1 +1,24 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_isPowerOfTwo(t *testing.T) {
+	tests := []struct {
+		args int
+		want bool
+	}{
+		{1, true},
+		{16, true},
+		{218, false},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			if got := isPowerOfTwo(tt.args); got != tt.want {
+				t.Errorf("isPowerOfTwo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
