8c7f21af286596354dcefa6a9653224d8f3c67f0
yidane <yidane@163.com>
2019-04-23 14:03:09 +0800

    update 217

--- leetcode/problems/217.contains-duplicate_1.go
+++ leetcode/problems/217.contains-duplicate_1.go
@@ -1,1 +1,55 @@
 package problems
+
+/*
+ * @lc app=leetcode id=217 lang=golang
+ *
+ * [217] Contains Duplicate
+ *
+ * https://leetcode.com/problems/contains-duplicate/description/
+ *
+ * algorithms
+ * Easy (51.47%)
+ * Total Accepted:    322.8K
+ * Total Submissions: 626.7K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * Given an array of integers, find if the array contains any duplicates.
+ *
+ * Your function should return true if any value appears at least twice in the
+ * array, and it should return false if every element is distinct.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,2,3,1]
+ * Output: true
+ *
+ * Example 2:
+ *
+ *
+ * Input: [1,2,3,4]
+ * Output: false
+ *
+ * Example 3:
+ *
+ *
+ * Input: [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ *
+ */
+func containsDuplicate(nums []int) bool {
+	if len(nums) <= 1 {
+		return false
+	}
+
+	m := make(map[int]bool, len(nums))
+	for i := 0; i < len(nums); i++ {
+		if m[nums[i]] {
+			return true
+		}
+
+		m[nums[i]] = true
+	}
+
+	return false
+}
--- leetcode/problems/217.contains-duplicate_1_test.go
+++ leetcode/problems/217.contains-duplicate_1_test.go
@@ -1,1 +1,24 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_containsDuplicate(t *testing.T) {
+	tests := []struct {
+		args []int
+		want bool
+	}{
+		{[]int{1, 2, 3, 1}, true},
+		{[]int{1, 2, 3, 4}, false},
+		{[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			if got := containsDuplicate(tt.args); got != tt.want {
+				t.Errorf("containsDuplicate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
