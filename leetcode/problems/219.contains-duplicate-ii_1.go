f7ab304fb5a234821ebbfe9fed1b87fa685d60d3
yidane <yidane@163.com>
2019-04-23 14:19:09 +0800

    update 219

--- leetcode/problems/219.contains-duplicate-ii_1.go
+++ leetcode/problems/219.contains-duplicate-ii_1.go
@@ -1,1 +1,64 @@
 package problems
+
+/*
+ * @lc app=leetcode id=219 lang=golang
+ *
+ * [219] Contains Duplicate II
+ *
+ * https://leetcode.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (35.03%)
+ * Total Accepted:    192K
+ * Total Submissions: 547.6K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * Given an array of integers and an integer k, find out whether there are two
+ * distinct indices i and j in the array such that nums[i] = nums[j] and the
+ * absolute difference between i and j is at most k.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ *
+ *
+ *
+ *
+ *
+ */
+func containsNearbyDuplicate(nums []int, k int) bool {
+	if len(nums) <= 1 {
+		return false
+	}
+
+	m := make(map[int]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		if idx, ok := m[nums[i]]; ok && i-idx <= k {
+			return true
+		}
+
+		m[nums[i]] = i
+	}
+
+	return false
+}
--- leetcode/problems/219.contains-duplicate-ii_1_test.go
+++ leetcode/problems/219.contains-duplicate-ii_1_test.go
@@ -1,1 +1,26 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_containsNearbyDuplicate(t *testing.T) {
+	tests := []struct {
+		args []int
+		k    int
+		want bool
+	}{
+		{[]int{1, 2, 3, 1}, 3, true},
+		{[]int{1, 0, 1, 1}, 1, true},
+		{[]int{1, 2, 3, 1, 2, 3}, 2, false},
+		{[]int{4, 1, 2, 3, 1, 5}, 3, true},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			if got := containsNearbyDuplicate(tt.args, tt.k); got != tt.want {
+				t.Errorf("containsNearbyDuplicate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
