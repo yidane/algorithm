84d59c328f86473e9a192c9bc83053dee1a0ce8f
yidane <yidane@163.com>
2019-04-23 13:40:18 +0800

    update 203

--- leetcode/problems/203.remove-linked-list-elements_1.go
+++ leetcode/problems/203.remove-linked-list-elements_1.go
@@ -1,1 +1,56 @@
 package problems
+
+/*
+ * @lc app=leetcode id=203 lang=golang
+ *
+ * [203] Remove Linked List Elements
+ *
+ * https://leetcode.com/problems/remove-linked-list-elements/description/
+ *
+ * algorithms
+ * Easy (35.57%)
+ * Total Accepted:    218.1K
+ * Total Submissions: 612.5K
+ * Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+ *
+ * Remove all elements from a linked list of integers that have value val.
+ *
+ * Example:
+ *
+ *
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+	for head != nil {
+		if head.Val != val {
+			break
+		}
+
+		head = head.Next
+	}
+
+	if head == nil {
+		return nil
+	}
+
+	node := head
+	for node != nil && node.Next != nil {
+		if node.Next.Val == val {
+			node.Next = node.Next.Next
+			continue
+		}
+		node = node.Next
+	}
+
+	return head
+}
--- leetcode/problems/203.remove-linked-list-elements_1_test.go
+++ leetcode/problems/203.remove-linked-list-elements_1_test.go
@@ -1,1 +1,28 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_removeElements(t *testing.T) {
+	tests := []struct {
+		args []int
+		val  int
+		want []int
+	}{
+		{[]int{1, 2, 6, 3, 4, 5, 6}, 6, []int{1, 2, 3, 4, 5}},
+		{[]int{1, 2, 2, 1}, 2, []int{1, 1}},
+		{[]int{1, 1, 1, 2, 3}, 1, []int{2, 3}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			head := NewListNode(tt.args)
+
+			got := removeElements(head, tt.val)
+			if !got.Equal(tt.want) {
+				t.Errorf("removeElements() = %v, want %v", got.ToArray(), tt.want)
+			}
+		})
+	}
+}
