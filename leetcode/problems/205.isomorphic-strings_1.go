6236f7ffa8e68c2f36f8f673c723e5c6360b6290
yidane <yidane@163.com>
2019-04-23 13:53:48 +0800

    update 205

--- leetcode/problems/205.isomorphic-strings_1.go
+++ leetcode/problems/205.isomorphic-strings_1.go
@@ -1,1 +1,77 @@
 package problems
+
+/*
+ * @lc app=leetcode id=205 lang=golang
+ *
+ * [205] Isomorphic Strings
+ *
+ * https://leetcode.com/problems/isomorphic-strings/description/
+ *
+ * algorithms
+ * Easy (37.04%)
+ * Total Accepted:    196.7K
+ * Total Submissions: 530.7K
+ * Testcase Example:  '"egg"\n"add"'
+ *
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ *
+ * All occurrences of a character must be replaced with another character while
+ * preserving the order of characters. No two characters may map to the same
+ * character but a character may map to itself.
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "egg", t = "add"
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ *
+ * Example 3:
+ *
+ *
+ * Input: s = "paper", t = "title"
+ * Output: true
+ *
+ * Note:
+ * You may assume both s and t have the same length.
+ *
+ */
+func isIsomorphic(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	ms := make(map[uint8]uint8, len(s))
+	mt := make(map[uint8]uint8, len(s))
+
+	for i := 0; i < len(s); i++ {
+		si := s[i]
+		ti := t[i]
+
+		if v, ok := ms[si]; ok {
+			if v != ti {
+				return false
+			}
+		} else {
+			ms[si] = ti
+		}
+
+		if v, ok := mt[ti]; ok {
+			if v != si {
+				return false
+			}
+		} else {
+			mt[ti] = si
+		}
+	}
+
+	return true
+}
--- leetcode/problems/205.isomorphic-strings_1_test.go
+++ leetcode/problems/205.isomorphic-strings_1_test.go
@@ -1,1 +1,26 @@
 package problems
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_isIsomorphic(t *testing.T) {
+	tests := []struct {
+		s    string
+		t    string
+		want bool
+	}{
+		{"egg", "add", true},
+		{"foo", "bar", false},
+		{"paper", "title", true},
+		{"ab", "aa", false},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.s, tt.t), func(t *testing.T) {
+			if got := isIsomorphic(tt.s, tt.t); got != tt.want {
+				t.Errorf("isIsomorphic() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
